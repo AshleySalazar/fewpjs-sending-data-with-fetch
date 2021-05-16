74ecdd8581c30e33c3d3981183c9bd5b786b8e62
Ashley Salazar <ashleysalazar0902@gmail.com>
2021-05-16 16:29:29 -0500

    commit tree 2c3c990dfa4d471712b0d81185b946e908c4f211

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+const submitData = (name, email) => {
+    const data = {
+        name,
+        email
+    };
+
+    const config = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+
+    return fetch('http://localhost:3000/users', config)
+        .then(res => res.json())
+        .then(result => {
+            return document.body.innerHTML += result.id
+        })
+        .catch(e => {
+            return document.body.innerHTML += 'Unauthorized Access'
+        })
+}
